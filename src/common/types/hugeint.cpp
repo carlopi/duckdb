53658939e775111d49b1c6b5c9baa5d717831ceb
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-01-27 12:53:10 +0100

    Fix warning: unary minus operator on unsigned

--- src/common/types/hugeint.cpp
+++ src/common/types/hugeint.cpp
@@ -398,7 +398,7 @@ bool HugeintTryCastInteger(hugeint_t input, DST &result) {
 		}
 		// negative number: check if the negative number is in range
 		if (input.lower >= NumericLimits<uint64_t>::Maximum() - uint64_t(NumericLimits<DST>::Maximum())) {
-			result = -DST(NumericLimits<uint64_t>::Maximum() - input.lower) - 1;
+			result = -1 -DST(NumericLimits<uint64_t>::Maximum() - input.lower);
 			return true;
 		}
 		break;
