ac7ba4a88ef2806557a19d81b629bdb393ab71a3
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-03-08 01:25:32 +0100

    conf

--- tools/nodejs/configure.py
+++ tools/nodejs/configure.py
@@ -22,71 +22,18 @@ import package_build
 
 defines = ['BUILD_{}_EXTENSION'.format(ext.upper()) for ext in extensions]
 
-if os.environ.get('DUCKDB_NODE_BUILD_CACHE') == '1' and os.path.isfile(cache_file):
-    with open(cache_file, 'rb') as f:
-        cache = pickle.load(f)
-    source_list = cache['source_list']
-    include_list = cache['include_list']
-    libraries = cache['libraries']
-    windows_options = cache['windows_options']
-    cflags = cache['cflags']
-elif 'DUCKDB_NODE_BINDIR' in os.environ:
-    def find_library_path(libdir, libname):
-        flist = os.listdir(libdir)
-        for fname in flist:
-            fpath = os.path.join(libdir, fname)
-            if os.path.isfile(fpath) and package_build.file_is_lib(fname, libname):
-                return fpath
-        raise Exception(f"Failed to find library {libname} in {libdir}")
-    # existing build
-    existing_duckdb_dir = os.environ['DUCKDB_NODE_BINDIR']
-    cflags = os.environ['DUCKDB_NODE_CFLAGS']
-    libraries = os.environ['DUCKDB_NODE_LIBS'].split(' ')
+# fresh build - copy over all of the files
+(source_list, include_list, original_sources) = package_build.build_package(target_dir, extensions, False)
 
-    include_directories = [os.path.join('..', '..', include) for include in package_build.third_party_includes()]
-    include_list = package_build.includes(extensions)
-
-    result_libraries = package_build.get_libraries(existing_duckdb_dir, libraries, extensions)
-    libraries = []
-    for (libdir, libname) in result_libraries:
-        if libdir is None:
-            continue
-        libraries.append(find_library_path(libdir, libname))
-
-    source_list = []
-    cflags = ['-frtti']
-    windows_options = []
-    if os.name == 'nt':
-        windows_options = [x for x in os.environ['DUCKDB_NODE_CFLAGS'].split(' ') if x.startswith('/')]
-    else:
-        if '-g' in os.environ['DUCKDB_NODE_CFLAGS']:
-            cflags += ['-g']
-        if '-O0' in os.environ['DUCKDB_NODE_CFLAGS']:
-            cflags += ['-O0']
-
-    if 'DUCKDB_NODE_BUILD_CACHE' in os.environ:
-        cache = {
-            'source_list': source_list,
-            'include_list': include_list,
-            'libraries': libraries,
-            'cflags': cflags,
-            'windows_options': windows_options
-        }
-        with open(cache_file, 'wb+') as f:
-            pickle.dump(cache, f)
-else:
-    # fresh build - copy over all of the files
-    (source_list, include_list, original_sources) = package_build.build_package(target_dir, extensions, False)
-
-    # # the list of all source files (.cpp files) that have been copied into the `duckdb_source_copy` directory
-    # print(source_list)
-    # # the list of all include files
-    # print(include_list)
-    source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x) for x in source_list]
-    include_list = [os.path.join('src', 'duckdb', x) for x in include_list]
-    libraries = []
-    windows_options = ['/GR']
-    cflags = ['-frtti']
+# # the list of all source files (.cpp files) that have been copied into the `duckdb_source_copy` directory
+# print(source_list)
+# # the list of all include files
+# print(include_list)
+source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x) for x in source_list]
+include_list = [os.path.join('src', 'duckdb', x) for x in include_list]
+libraries = []
+windows_options = ['/GR']
+cflags = ['-frtti','-v']
 
 def sanitize_path(x):
     return x.replace('\\', '/')
