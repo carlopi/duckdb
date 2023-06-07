8ac2b61aacc6b51a4ec2829e7577709d7d9d088e
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-06-07 15:14:22 +0200

    Function serialization: duplicate classes to take either state or context

--- src/include/duckdb/function/function_serialization.hpp
+++ src/include/duckdb/function/function_serialization.hpp
@@ -74,12 +74,49 @@ public:
 		return function;
 	}
 	template <class FUNC, class CATALOG_ENTRY>
+	static FUNC DeserializeBaseInternal(FieldReader &reader, ClientContext &context, CatalogType type,
+	                                    unique_ptr<FunctionData> &bind_info, bool &has_deserialize) {
+		auto name = reader.ReadRequired<string>();
+		auto arguments = reader.ReadRequiredSerializableList<LogicalType, LogicalType>();
+		// note: original_arguments are optional (can be list of size 0)
+		auto original_arguments = reader.ReadRequiredSerializableList<LogicalType, LogicalType>();
+
+		auto &func_catalog = Catalog::GetEntry(context, type, SYSTEM_CATALOG, DEFAULT_SCHEMA, name);
+		if (func_catalog.type != type) {
+			throw InternalException("Cant find catalog entry for function %s", name);
+		}
+
+		auto &functions = func_catalog.Cast<CATALOG_ENTRY>();
+		auto function = functions.functions.GetFunctionByArguments(
+		    context, original_arguments.empty() ? arguments : original_arguments);
+		function.arguments = std::move(arguments);
+		function.original_arguments = std::move(original_arguments);
+
+		has_deserialize = reader.ReadRequired<bool>();
+		if (has_deserialize) {
+			if (!function.deserialize) {
+				throw SerializationException("Function requires deserialization but no deserialization function for %s",
+				                             function.name);
+			}
+			bind_info = function.deserialize(context, reader, function);
+		} else {
+			D_ASSERT(!function.serialize);
+			D_ASSERT(!function.deserialize);
+		}
+		return function;
+	}
+	template <class FUNC, class CATALOG_ENTRY>
 	static FUNC DeserializeBase(FieldReader &reader, PlanDeserializationState &state, CatalogType type,
 	                            unique_ptr<FunctionData> &bind_info) {
 		bool has_deserialize;
 		return DeserializeBaseInternal<FUNC, CATALOG_ENTRY>(reader, state, type, bind_info, has_deserialize);
 	}
-
+	template <class FUNC, class CATALOG_ENTRY>
+	static FUNC DeserializeBase(FieldReader &reader, ClientContext &context, CatalogType type,
+	                            unique_ptr<FunctionData> &bind_info) {
+		bool has_deserialize;
+		return DeserializeBaseInternal<FUNC, CATALOG_ENTRY>(reader, context, type, bind_info, has_deserialize);
+	}
 	template <class FUNC, class CATALOG_ENTRY>
 	static FUNC Deserialize(FieldReader &reader, ExpressionDeserializationState &state, CatalogType type,
 	                        vector<unique_ptr<Expression>> &children, unique_ptr<FunctionData> &bind_info) {
@@ -91,6 +128,22 @@ public:
 
 		// we re-bind the function only if the function did not have an explicit deserialize method
 		auto &context = state.gstate.context;
+		if (!has_deserialize && function.bind) {
+			bind_info = function.bind(context, function, children);
+		}
+		function.return_type = return_type;
+		return function;
+	}
+	template <class FUNC, class CATALOG_ENTRY>
+	static FUNC Deserialize(FieldReader &reader, ClientContext& context, CatalogType type,
+	                        vector<unique_ptr<Expression>> &children, unique_ptr<FunctionData> &bind_info) {
+		bool has_deserialize;
+		auto function =
+		    DeserializeBaseInternal<FUNC, CATALOG_ENTRY>(reader, context, type, bind_info, has_deserialize);
+		auto return_type = reader.ReadRequiredSerializable<LogicalType, LogicalType>();
+		children = reader.ReadRequiredSerializableList<Expression>(context);
+
+		// we re-bind the function only if the function did not have an explicit deserialize method
 		if (!has_deserialize && function.bind) {
 			bind_info = function.bind(context, function, children);
 		}
