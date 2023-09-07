63123450c59da2c9251f8222ee6d80ded8e97e5d
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-09-07 14:34:48 +0200

    Extensions: Add placeholder extension with error 'This is a placeholder DuckDB extension'

---
+++ extension/placeholder/placeholder_extension.cpp
@@ -0,0 +1,15 @@
+#define DUCKDB_EXTENSION_MAIN
+
+#include "duckdb/main/extension_util.hpp"
+
+extern "C" {
+
+DUCKDB_EXTENSION_API const char *error_message() {
+	return "This is a placeholder DuckDB extension";
+}
+
+}
+
+#ifndef DUCKDB_EXTENSION_MAIN
+#error DUCKDB_EXTENSION_MAIN not defined
+#endif
