e40e2cfb85a2228bcc708cd34f4d67d1fe1eb758
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-11-14 00:33:22 +0100

    maybe??

--- tools/pythonpkg/setup.py
+++ tools/pythonpkg/setup.py
@@ -353,7 +353,7 @@ setup(
     packages=packages,
     include_package_data=True,
     python_requires='>=3.9.0',
-    setup_requires=setup_requires + ["setuptools_scm<7.0.0", 'pybind11>=2.6.0'],
+    setup_requires=setup_requires + ["setuptools_scm", 'pybind11>=2.6.0'],
     use_scm_version=setuptools_scm_conf,
     tests_require=['google-cloud-storage', 'mypy', 'pytest'],
     classifiers=[
