e83cdf0a26383df0cf7763ccaab6f3a1173a621c
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-09 16:01:59 +0200

    macro function

--- src/include/duckdb/function/macro_function.hpp
+++ src/include/duckdb/function/macro_function.hpp
@@ -30,7 +30,8 @@ public:
 	//! The default parameters and their associated values
 	case_insensitive_map_t<unique_ptr<ParsedExpression>> default_parameters;
 
-	const char* extension{NULL};
+	const char *extension {NULL};
+
 public:
 	virtual ~MacroFunction() {
 	}
