3b9a0349c61f7ea4550df214a7593a75e4b248b1
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-24 11:16:07 +0200

    scan pl dataframe

--- examples/example4.py
+++ examples/example4.py
@@ -1,6 +1,10 @@
-import polars
+import polars as pl
 import duckdb
+
+pldf = pl.DataFrame({'mynum': [1,2,3,4]})
+
+duckdb.sql("SELECT * FROM pldf").show()
+
 con = duckdb.connect("file.db")
-con.sql("CREATE TABLE integers (i INTEGER)")
-con.sql("INSERT INTO integers VALUES (42)")
+con.sql("CREATE TABLE integers AS (FROM pldf)")
 con.sql("SELECT * FROM integers").show()
