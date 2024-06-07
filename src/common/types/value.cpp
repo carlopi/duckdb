96c1414bb3d4568e82cae2b0d0ee8f27fe7a3cf5
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-06-07 10:09:04 +0200

    Revert "throw non-fatal exception and add assertions"
    This reverts commit 2d9b1263ca2ce06a5ebe2a468b997c1e6759f63f.

--- src/common/types/value.cpp
+++ src/common/types/value.cpp
@@ -346,10 +346,8 @@ Value Value::MaximumValue(const LogicalType &type) {
 			throw InternalException("Unknown decimal type");
 		}
 	}
-	case LogicalTypeId::ENUM: {
-		D_ASSERT(EnumType::GetSize(type) > 0);
+	case LogicalTypeId::ENUM:
 		return Value::ENUM(EnumType::GetSize(type) - 1, type);
-	}
 	default:
 		throw InvalidTypeException(type, "MaximumValue requires numeric type");
 	}
