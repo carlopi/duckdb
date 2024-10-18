79727211909c3da3213a2a044219c465114ffe60
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-10-18 12:58:19 +0200

    Some more headers

--- src/include/duckdb/execution/operator/aggregate/ungrouped_aggregate_state.hpp
+++ src/include/duckdb/execution/operator/aggregate/ungrouped_aggregate_state.hpp
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "duckdb/common/mutex.hpp"
+#include "duckdb/common/atomic.hpp"
 #include "duckdb/execution/expression_executor.hpp"
 #include "duckdb/function/aggregate_function.hpp"
 #include "duckdb/planner/expression/bound_aggregate_expression.hpp"
