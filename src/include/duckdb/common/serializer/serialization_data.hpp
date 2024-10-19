8de242ffdde47b47eec37969bec961e382eba73f
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-10-19 09:10:08 +0200

    duckdb/common/helper.hpp

--- src/include/duckdb/common/serializer/serialization_data.hpp
+++ src/include/duckdb/common/serializer/serialization_data.hpp
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "duckdb/common/common.hpp"
+#include "duckdb/common/helper.hpp"
 #include "duckdb/common/stack.hpp"
 #include "duckdb/planner/bound_parameter_map.hpp"
 #include "duckdb/function/compression_function.hpp"
