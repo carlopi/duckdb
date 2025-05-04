534124e0f6efc8ae54c15ed93fc23a08d9564258
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-05-04 04:15:04 +0200

    Fixup BufferedFileHandle

--- src/common/buffered_file_handle.cpp
+++ src/common/buffered_file_handle.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 namespace duckdb {
 
-BufferedFileHandle::BufferedFileHandle(FileHandle &inner_handle, size_t start, size_t end, Allocator &allocator) : inner_handle(inner_handle), start(start), end(end), allocator(allocator) {
+BufferedFileHandle::BufferedFileHandle(FileHandle &inner_handle, size_t start, size_t end, Allocator &allocator)
+    : inner_handle(inner_handle), start(start), end(end), allocator(allocator) {
 	if (start > end) {
 		throw InvalidInputException("Range in BufferedFileHandle constructor wrongly set");
 	}
 	buffered = allocator.Allocate(end - start);
 
-	inner_handle.Read(buffered.get(),  end - start, start);
+	inner_handle.Read(buffered.get(), end - start, start);
 }
 
 BufferedFileHandle::~BufferedFileHandle() {
@@ -20,26 +21,21 @@ BufferedFileHandle::~BufferedFileHandle() {
 void BufferedFileHandle::Read(void *buffer, idx_t nr_bytes, idx_t location) {
 	std::cout << "BufferedFileHandle::Read " << nr_bytes << " from  " << location << "\n";
 	data_ptr_t ptr = static_cast<data_ptr_t>(buffer);
-/*
-	idx_t original_location = location;
-	if (location < start) {
-		inner_handle.Read(ptr, start-location, location);
-		ptr += start-location;
-		location = start;
+	idx_t current_location = location;
+
+	if (current_location < start) {
+		inner_handle.Read(ptr, start - current_location, current_location);
+		ptr += start - current_location;
+		current_location = start;
 	}
-	if (location < end) {
-*/
-		std::cout << (int)buffered.get()[8] << (int)buffered.get()[9] << (int)buffered.get()[10] <<(int) buffered.get()[11] << "\n";
-		memcpy(ptr, buffered.get() + location, nr_bytes);
-		std::cout << (int)ptr[0] << (int)ptr[1] << (int)ptr[2] <<(int) ptr[3] << "\n";
+	if (current_location < end) {
+		memcpy(ptr, buffered.get() + current_location, nr_bytes);
 		ptr += nr_bytes;
-		// FIXME, copy from buffer
-/*
+		current_location += nr_bytes;
 	}
-	if (original_location + nr_bytes > end) {
-		inner_handle.Read(ptr, end, original_location+nr_bytes - end);
+	if (location + nr_bytes > end) {
+		inner_handle.Read(ptr, end - current_location, current_location);
 	}
-*/
 }
 
 idx_t BufferedFileHandle::GetFileSize() {
@@ -50,4 +46,4 @@ string BufferedFileHandle::GetPath() {
 	return inner_handle.GetPath();
 }
 
-}; // namespace
+}; // namespace duckdb
