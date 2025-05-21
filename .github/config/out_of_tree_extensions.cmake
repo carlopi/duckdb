45f9d580b8d19fc3f402a480f17bfd3e822bd6bc
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-05-21 07:40:08 +0200

    Bump spatial after wasm build fix

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -115,7 +115,7 @@ if (NOT MINGW)
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG 494d94912cc7ebcd8c43c9b6fc173a3e4142740f
+    GIT_TAG 450094cfa48b0485b55c096d280dbb0fe9185e82
     INCLUDE_DIR spatial/include
     TEST_DIR test/sql
     )
