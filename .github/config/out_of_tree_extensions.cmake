ae5aec351720c5d29eb21bc29fef765cf5d90a77
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-07-19 19:05:15 +0200

    patch spatial tests (??)

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -120,6 +120,7 @@ endif()
 if (NOT MINGW AND ${BUILD_COMPLETE_EXTENSION_SET})
 ################# SPATIAL
 duckdb_extension_load(spatial
+	APPLY_PATCHES
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
     GIT_TAG bef22c4b109d235c2b5725ff9ccf191485d14fd8
