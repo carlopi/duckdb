dd843046b69832e6b7fa105b8ae9db1b0fb30cfe
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-10-14 21:11:01 +0200

    Revert "More remove logging"
    This reverts commit 40f5ced70fcb1a1b3a0c312e3d7ab00d86c5b72d.

--- src/execution/operator/scan/physical_table_scan.cpp
+++ src/execution/operator/scan/physical_table_scan.cpp
@@ -147,6 +147,7 @@ go_back:
 
 				TaskExecutor executor(context.client);
 
+				std::cout << res->v.size() << "\n";
 				for (auto &promise : res->v) {
 					auto task = make_uniq<PromiseExecutionTask>(executor, std::move(promise));
 					executor.ScheduleTask(std::move(task));
