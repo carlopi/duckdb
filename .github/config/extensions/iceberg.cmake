30ae8137faeb953a9eb345760c13944942730413
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-11-03 17:38:31 +0100

    Add back tests also for iceberg

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -6,7 +6,7 @@ else ()
 endif()
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(iceberg
-#            ${LOAD_ICEBERG_TESTS} TODO: re-enable once autoloading test is fixed
+            ${LOAD_ICEBERG_TESTS}
             GIT_URL https://github.com/duckdb/duckdb-iceberg
             GIT_TAG 30a2c66f1040ffd36e117ace6e22acbdb402e79c
             )
