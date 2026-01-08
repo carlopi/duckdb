fc253624f05d02543dd9bbc9cf15f426218daeb9
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-01-08 20:38:37 +0100

    Bump unity

--- .github/config/extensions/unity_catalog.cmake
+++ .github/config/extensions/unity_catalog.cmake
@@ -2,7 +2,7 @@ if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
     duckdb_extension_load(unity_catalog
             DONT_LINK
             GIT_URL https://github.com/duckdb/unity_catalog
-            GIT_TAG 1ad4f0b1fb12f661ff58fc91c2c9e7022591ea4b
+            GIT_TAG 8a65e9c446cfaa8e3e9502007ecac10190bbe91d
             LOAD_TESTS
             )
 endif()
