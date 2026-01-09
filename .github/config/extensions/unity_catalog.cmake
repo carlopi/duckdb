37a1dfcba8d4cd228eea1fcb21f15dfddcb246de
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-01-09 09:21:51 +0100

    For now skip tests

--- .github/config/extensions/unity_catalog.cmake
+++ .github/config/extensions/unity_catalog.cmake
@@ -3,6 +3,5 @@ if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
             DONT_LINK
             GIT_URL https://github.com/duckdb/unity_catalog
             GIT_TAG 8a65e9c446cfaa8e3e9502007ecac10190bbe91d
-            LOAD_TESTS
             )
 endif()
