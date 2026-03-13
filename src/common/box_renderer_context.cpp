e11e30758be813136d69273f3c1385b6cf78e56c
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-03-13 15:28:30 +0100

    Split ClientBoxRendererContext into its own file to fix wire mode link
    ClientBoxRendererContext depends on ClientContext (engine). Keeping it
    in box_renderer_context.cpp pulled the symbol into duckdb_client_types.

--- src/common/box_renderer_context.cpp
+++ src/common/box_renderer_context.cpp
@@ -1,12 +1,8 @@
 #include "duckdb/common/box_renderer_context.hpp"
-#include "duckdb/main/client_context.hpp"
 #include "duckdb/common/types/vector.hpp"
 #include "duckdb/common/types/data_chunk.hpp"
-#include "duckdb/common/vector_operations/vector_operations.hpp"
 
 namespace duckdb {
-
-// ===== BoxRendererContext (base) =====
 
 void BoxRendererContext::CastToVarchar(Vector &source, Vector &result, idx_t count, bool as_json) {
 	DataChunk source_chunk;
@@ -20,25 +16,6 @@ void BoxRendererContext::CastToVarchar(Vector &source, Vector &result, idx_t cou
 	result_chunk.SetCardinality(count);
 
 	CastToVarchar(source_chunk, result_chunk, count, as_json);
-}
-
-// ===== ClientBoxRendererContext =====
-
-ClientBoxRendererContext::ClientBoxRendererContext(ClientContext &context) : context(context) {
-}
-
-bool ClientBoxRendererContext::IsInterrupted() const {
-	return context.IsInterrupted();
-}
-
-void ClientBoxRendererContext::CastToVarchar(DataChunk &source, DataChunk &result, idx_t count, bool as_json) {
-	for (idx_t c = 0; c < source.ColumnCount(); c++) {
-		VectorOperations::TryCast(context, source.data[c], result.data[c], count, nullptr, false);
-	}
-}
-
-Allocator &ClientBoxRendererContext::GetAllocator() {
-	return Allocator::Get(context);
 }
 
 } // namespace duckdb
