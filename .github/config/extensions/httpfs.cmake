a0278e66a6535b342b5aaa61278b62e1679335f7
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-04-23 09:02:54 +0200

    Move to carlopi's httpfs with user-agent bug-hunt

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
-    GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 13e18b3c9f3810334f5972b76a3acc247b28e537
+    GIT_URL https://github.com/carlopi/duckdb-httpfs
+    GIT_TAG 6e53263940774daa913616a59825d70970651f30
 )
