bd55eb0e36964bd1df1fb98af8b54a8b8d20c947
carlo <piovesan.carlo@gmail.com>
2026-09-08 18:23:22 +0200

    bump extensions

--- .github/config/extensions/fts.cmake
+++ .github/config/extensions/fts.cmake
@@ -2,7 +2,7 @@ duckdb_extension_load(fts
         LOAD_TESTS
         DONT_LINK
         GIT_URL https://github.com/duckdb/duckdb-fts
-        GIT_TAG 2266e67d01c737cb65114e77c7c672e58869a635
+        GIT_TAG 513280f7de1c5e32171076ce2b62d5200cf64621
         TEST_DIR test/sql
         APPLY_PATCHES
 )
--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 984d576bc696248af5cc51f9ec4c2f2c8a9dbd1f
+    GIT_TAG a36f1211ad59f14dc2beac577c39432df7353a6d
 )
